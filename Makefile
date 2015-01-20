f112ff6cced0c58bda77ecbcf85208faff9cd4a5
Andrea Zoli <zoli@iasfbo.inaf.it>
2015-01-20 14:17:59 +0100

    Add OpenMPI versions.

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-all: streamer receiver_protobuf receiver_packetlib
+all: streamer receiver_protobuf receiver_packetlib packetlib_mpi protobuf_mpi
+
+MPIFLAGS=$(shell mpic++ --showme:compile)
+MPIFLAGS+= $(shell mpic++ --showme:link)
 
 streamer: streamer.cpp
 	g++ -O2 -std=c++11 -Wall streamer.cpp -o streamer -lCTAToolsCore -lpacket -lprotobuf -lzmq
@@ -9,5 +12,11 @@ receiver_protobuf: receiver_protobuf.cpp
 receiver_packetlib: receiver_packetlib.cpp
 	g++ -O2 -std=c++11 -Wall receiver_packetlib.cpp -o receiver_packetlib -lcfitsio -lCTAConfig -lCTAToolsCore -lRTAUtils -lpacket -lprotobuf -lzmq
 
+packetlib_mpi: packetlib_mpi.cpp
+	mpic++ -std=c++11 -O2 -Wall packetlib_mpi.cpp -o packetlib_mpi -lcfitsio -lCTAConfig -lRTAUtils -lpacket $(MPIFLAGS)
+
+protobuf_mpi: protobuf_mpi.cpp
+	mpic++ -std=c++11 -O2 -Wall protobuf_mpi.cpp -o protobuf_mpi -lcfitsio -lCTAConfig -lRTAUtils -lCTAToolsCore -lpacket -lprotobuf $(MPIFLAGS)
+
 clean:
-	rm -f streamer receiver_protobuf receiver_packetlib
+	rm -f streamer receiver_protobuf receiver_packetlib packetlib_mpi protobuf_mpi
---
+++ packetlib_mpi.cpp
@@ -0,0 +1,127 @@
+/***************************************************************************
+ PacketLib .packet streamer/receiver using OpenMPI.
+ ---------------------------------------------------------------------------
+ copyright            : (C) 2014 Andrea Zoli
+ email                : zoli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
+#include <mpi.h>
+#include <iostream>
+#include <packet/Packet.h>
+#include <packet/PacketStream.h>
+#include <packet/PacketBufferV.h>
+#include <CTAMDArray.h>
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
+
+
+
+int main(int argc, char* argv[])
+{
+	if(argc <= 3)
+	{
+		std::cout << "Wrong arguments, please provide config file, raw input file and number of packets." << std::endl;
+		std::cout << argv[0] << " <configfile> <inputfile> <npackets>" << std::endl;
+		return 1;
+	}
+	const std::string configFilename = argv[1];
+	const std::string inputFilename = argv[2];
+	const unsigned long numevents = std::atol(argv[3]);
+
+	// init MPI
+	MPI::Init();
+	int size = MPI::COMM_WORLD.Get_size();
+	int rank = MPI::COMM_WORLD.Get_rank();
+	char hostname[MPI_MAX_PROCESSOR_NAME];
+	int hostnameLen;
+	MPI::Get_processor_name(hostname, hostnameLen);
+	std::cout << "host: " << hostname << " size: " << size << " rank: " << rank << std::endl;
+
+	if(rank == 0) // streamer
+	{
+		// load events buffer
+		PacketLib::PacketBufferV events(configFilename, inputFilename);
+		events.load();
+
+		// send events
+		unsigned int idx = 0, counter = 0;
+		while(counter++ < numevents)
+		{
+			PacketLib::ByteStreamPtr event = events.getNext();
+			// send number of events
+			unsigned long eventSize = event->size();
+			MPI::COMM_WORLD.Send(&eventSize, 1, MPI_UNSIGNED_LONG, 1, 0);
+			MPI::COMM_WORLD.Send(event->getStream(), eventSize, MPI_UNSIGNED_CHAR, 1, 0);
+
+			idx = (idx+1) % events.size();
+		}
+	}
+	else if(rank == 1) // receiver
+	{
+		CTAConfig::CTAMDArray array_conf;
+		std::cout << "Preloading.." << std::endl;
+		array_conf.loadConfig("AARPROD2", "PROD2_telconfig.fits.gz", "Aar.conf", "conf/");
+		std::cout << "Load complete!" << std::endl;
+
+		unsigned long message_count = 0;
+		unsigned long message_size = 0;
+
+		PacketLib::PacketStream ps(configFilename.c_str());
+
+		PacketLib::byte event[500000];
+		unsigned long eventSize = 0;
+
+		MPI::COMM_WORLD.Recv(&eventSize, 1, MPI_UNSIGNED_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG);
+
+		std::chrono::time_point<std::chrono::system_clock> start, end;
+		start = std::chrono::system_clock::now();
+		while(message_count < numevents)
+		{
+			if(message_count > 0)
+				MPI::COMM_WORLD.Recv(&eventSize, 1, MPI_UNSIGNED_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG);
+			MPI::COMM_WORLD.Recv(&event, eventSize, MPI_UNSIGNED_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG);
+
+			message_count++;
+			message_size += eventSize;
+
+			/// decoding packetlib packet
+			PacketLib::ByteStreamPtr stream = PacketLib::ByteStreamPtr(new PacketLib::ByteStream(event, eventSize, false));
+			PacketLib::Packet *packet = ps.getPacket(stream);
+
+			/// get telescope id
+			PacketLib::DataFieldHeader* dfh = packet->getPacketDataFieldHeader();
+			const unsigned int telescopeID = dfh->getFieldValue_16ui("TelescopeID");
+
+			/// get the waveforms
+			PacketLib::byte* buff = packet->getData()->getStream();
+			PacketLib::dword buffSize = packet->getData()->size();
+
+			/// get npixels and nsamples from ctaconfig using the telescopeID
+			CTAConfig::CTAMDTelescopeType* teltype = array_conf.getTelescope(telescopeID)->getTelescopeType();
+			int telTypeSim = teltype->getID();
+			const unsigned int npixels = teltype->getCameraType()->getNpixels();
+			const unsigned int nsamples = teltype->getCameraType()->getPixel(0)->getPixelType()->getNSamples();
+		}
+
+		end = std::chrono::system_clock::now();
+		std::chrono::duration<double> elapsed = end-start;
+		double msgs = message_count / elapsed.count();
+		double mbits = ((message_size / 1000000) * 8) / elapsed.count();
+		std::cout << message_count << "messages sent in " << elapsed.count() << " s" << std::endl;
+		std::cout << "mean message size: " << message_size / message_count << std::endl;
+		std::cout << "throughput: " << msgs << " msg/s = " << mbits << " mbit/s" << std::endl;
+	}
+
+	MPI::Finalize();
+
+	return 0;
+}
